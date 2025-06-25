f815e87a6e249b1a80e7af6c7e7815c329f436a6
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-06-24 19:04:17 -0600

    ✨ Define User entity with properties and UserRole enum for role management

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  STUDENT = 'student',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.STUDENT,
+  })
+  role: UserRole;
+}
