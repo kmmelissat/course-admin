9be290e309a78349a5885aaff14aa795d4e25542
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-06-24 19:05:09 -0600

    ✨ Implement Course entity with properties for id, title, description, and price

--- src/course/entities/course.entity.ts
+++ src/course/entities/course.entity.ts
@@ -1,1 +1,17 @@
-export class Course {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('courses')
+export class Course {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+}
