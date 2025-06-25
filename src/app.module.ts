d1d20a2318e72190e78bb4b2aa6f529323e5c09a
Melissa Solórzano <163376726+kmmelissat@users.noreply.github.com>
2025-06-24 19:00:09 -0600

    ✨ Add TypeORM configuration and global validation pipe to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CourseModule } from './course/course.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from './course/entities/course.entity';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [CourseModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'course-api',
+      entities: [Course, User],
+      synchronize: true,
+    }),
+    CourseModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
